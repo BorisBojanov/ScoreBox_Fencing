25a5e062b3a619abbaf02e6271b4e4bec056ba66
BorisBojanov <boris.bojanov@gmail.com>
2025-07-08 06:58:53 -0600

    Update GPIO_Checker.cpp

--- GPIO_Checker.cpp
+++ GPIO_Checker.cpp
@@ -19,6 +19,20 @@ bool GPIO_Checker::resetPins(){
 
 void GPIO_Checker::initialize(){
     // Initialize GPIO pins, set modes, etc.
+    // Initialize pins for ESP32
+    // pinMode(RED_WEAPON_PIN, INPUT_PULLUP);
+    // pinMode(GREEN_WEAPON_PIN, INPUT_PULLUP);
+    // pinMode(RED_LED_PIN, OUTPUT);
+    // pinMode(GREEN_LED_PIN, OUTPUT);
+    // pinMode(BUZZER_PIN, OUTPUT);
+    // pinMode(RESET_BUTTON_PIN, INPUT_PULLUP);
+    setPinNumber(RED_WEAPON_PIN_A, 2);
+    setPinNumber(RED_WEAPON_PIN_B, 3);
+    setPinNumber(RED_WEAPON_PIN_C, 4);
+    setPinNumber(GREEN_WEAPON_PIN_A, 5);
+    setPinNumber(GREEN_WEAPON_PIN_B, 6);
+    setPinNumber(GREEN_WEAPON_PIN_C, 7);
+    
     cout << "GPIO Checker initialized." << endl;
 }
 
@@ -86,4 +100,25 @@ int GPIO_Checker::getPinType(int pinNumber){
     }
     // if not found, throw an exception
     throw std::invalid_argument("Pin number not found in pinNumbers array.");
+}
+
+// Get the pin readings for the fencer side (Red or Green).
+// also includes time of readings
+// Returns an array of pin readings based on the fencer side (Red or Green).
+int* GPIO_Checker::getPinReadings(string RedOrGreen) {
+    static int readings[4]; // Assuming 3 pins for each weapon
+    if (RedOrGreen == "Red") {
+        readings[0] = pinNumbers[RED_WEAPON_PIN_A];
+        readings[1] = pinNumbers[RED_WEAPON_PIN_B];
+        readings[2] = pinNumbers[RED_WEAPON_PIN_C];
+        readings[3] = redHitStartTime; // Include the time of the hit
+    } else if (RedOrGreen == "Green") {
+        readings[0] = pinNumbers[GREEN_WEAPON_PIN_A];
+        readings[1] = pinNumbers[GREEN_WEAPON_PIN_B];
+        readings[2] = pinNumbers[GREEN_WEAPON_PIN_C];
+        readings[3] = greenHitStartTime; // Include the time of the hit
+    } else {
+        throw std::invalid_argument("Invalid weapon type specified.");
+    }
+    return readings;
 }
